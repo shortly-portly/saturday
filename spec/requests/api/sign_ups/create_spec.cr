44e01952f3c4b3995f9d4e2daff465b327cea81a
Dave Simmons <shortlyportly@googlemail.com>
2019-12-29 14:12:59 +0000

    updated tests to use new user.role

--- spec/requests/api/sign_ups/create_spec.cr
+++ spec/requests/api/sign_ups/create_spec.cr
@@ -30,5 +30,6 @@ private def valid_params
     email:                 "test@email.com",
     password:              "password",
     password_confirmation: "password",
+    role:                  0,
   }
 end
--- spec/support/boxes/user_box.cr
+++ spec/support/boxes/user_box.cr
@@ -2,5 +2,6 @@ class UserBox < Avram::Box
   def initialize
     email "#{sequence("test-email")}@example.com"
     encrypted_password Authentic.generate_encrypted_password("password")
+    role 0
   end
 end
